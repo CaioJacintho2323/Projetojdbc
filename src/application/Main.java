487697f22ff1d8babfaca01983310ce561d651b3
CaioJacintho2323 <jacinthocaio@gmail.com>
2024-05-22 14:29:09 -0300

    add seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,14 @@ package application;
 
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Departament d1 = new Departament(1, "livros");
-        System.out.println(d1);
+        Seller sc = new Seller(21, "bob", "bob@gmail", new Date(),3000.0,d1);
+        System.out.println(sc);
     }
 }
