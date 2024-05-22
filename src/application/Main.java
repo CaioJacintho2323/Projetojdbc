cbc1e3401e3b07143720b154f583ef106d7770af
CaioJacintho2323 <jacinthocaio@gmail.com>
2024-05-22 14:18:32 -0300

    add entities e application

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+
+import model.entities.Departament;
+
+public class Main {
+    public static void main(String[] args) {
+        Departament d1 = new Departament(1, "livros");
+        System.out.println(d1);
+    }
+}
